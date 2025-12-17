9e15d22095d21aa1dbd0b0c0ccb6a9da3e0ae6a5
shaiksanda <shaiksandanibasha7@gmail.com>
2025-12-17 08:43:33 +0530

    fix(snippets): move /all-snippets above /:snippetId

--- backend/routes/snippetRoutes.js
+++ backend/routes/snippetRoutes.js
@@ -5,8 +5,8 @@ const authenticateUser=require('../middlewares/auth')
 const {createSnippet,getSnippet,getAllSnippets,updateSnippet,deleteSnippet} =require("../controllers/snippet")
 
 router.post("/create",authenticateUser,createSnippet)
-router.get("/:snippetId",authenticateUser,getSnippet)
 router.get('/all-snippets',authenticateUser,getAllSnippets)
+router.get("/:snippetId",authenticateUser,getSnippet)
 router.put("/update/:snippetId",authenticateUser,updateSnippet)
 router.delete("/delete/:snippetId",authenticateUser,deleteSnippet)
 
